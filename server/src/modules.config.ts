38cf918150f61af613143f1401b7aa188e863c16
HysenB <hysi2000@live.com>
2024-03-18 19:48:43 +0100

    jwt to sign accesstoken

--- server/src/modules.config.ts
+++ server/src/modules.config.ts
@@ -32,3 +32,14 @@ export const redisModule = RedisModule.registerAsync({
     },
     inject: [ConfigService],
 })
+
+export const jwtModule = JwtModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+            expiresIn: parseInt(configService.get<string>('POLL_DURATION'))
+        }
+    }),
+    inject: [ConfigService]
+})
