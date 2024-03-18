b0288f0f0f287224cdf8942e41b5361b65324967
HysenB <hysi2000@live.com>
2024-03-18 19:39:16 +0100

    polls repository

---
+++ shared/poll-types.ts
@@ -0,0 +1,16 @@
+
+export interface Participants {
+    [participantID: string]: string;
+}
+
+export interface Poll {
+    id: string;
+    topic: string;
+    votesPerVoter: number;
+    participants: Participants;
+    adminID: string;
+    // nominations: Nominations;
+    // rankings: Rankings;
+    // results: Results;
+    // hasStarted: boolean;
+}
