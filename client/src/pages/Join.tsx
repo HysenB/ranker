bc83f4967ce50ef02c34287f1663ce150e5fbf0c
HysenB <hysi2000@live.com>
2024-05-07 20:08:34 +0200

    small fix qout to quot

--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Join: React.FC = () => {
     return (
         <div className="flex flex-col items-stretch justify-around w-full h-full max-w-sm mx-auto">
-            <h3 className="text-center">Enter Code Provided by &qout; Friend &qout;</h3>
+            <h3 className="text-center">Enter Code Provided by &quot;Friend&quot;</h3>
         </div>
     )
 }
